5e1f9e898ec463252a11f2ed4d12f9e110918fde
BLOSSOM <ELIZA.HERBISON94@GMAIL.COM>
2025-08-19 20:30:23 +0800

    views, urls and models for user

--- crowdfunding/fundraisers/models.py
+++ crowdfunding/fundraisers/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
 class Fundraiser(models.Model):
    title = models.CharField(max_length=200)
    description = models.TextField()
@@ -8,6 +9,11 @@ class Fundraiser(models.Model):
    image = models.URLField()
    is_open = models.BooleanField()
    date_created = models.DateTimeField(auto_now_add=True)
+   owner = models.ForeignKey(
+   get_user_model(),
+   related_name='owned_fundraisers',
+   on_delete=models.CASCADE    
+   )
 
 class Pledge(models.Model):
    amount = models.IntegerField()
@@ -18,4 +24,9 @@ class Pledge(models.Model):
       related_name='pledges',
       on_delete=models.CASCADE
    )
-
+supporter = models.ForeignKey(
+   get_user_model(),
+   related_name='pledges',
+   on_delete=models.CASCADE      
+   
+)
